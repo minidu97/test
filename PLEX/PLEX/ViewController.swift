b5c8e6d44cb12076856fadb52f8b2d926ed3ee9b
minidu97 <42093242+minidu97@users.noreply.github.com>
2021-01-28 19:03:19 +0530

    Add files via upload

---
+++ PLEX/PLEX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PLEX
+//
+//  Created by Minidu Wickramaarachchi on 1/22/21.
+//  Copyright © 2021 Minidu Wickramaarachchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
